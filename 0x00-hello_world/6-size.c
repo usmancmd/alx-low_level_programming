c6137083eb911e899e35229e6d9d268cee6739df
Usman Adepoju <adepojuusman@gmail.com>
2022-09-09 14:47:51 -0700

    4-puts.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,10 @@
  */
 int main(void)
 {
-printf("size of a char:%i bytes(s)\n",sizeof(char));
-printf("size of a int:%i byte(s)\n",sizeof(int));
-printf("size of a long int:%i byte(s)\n",sizeof(long int));
-printf("size of a long long int:%i byte(s)\n",sizeof(long long int));
-printf("size of a float:%i byte(s)\n",sizeof(float));
+printf("size of a char:%i bytes(s)\n", sizeof(char));
+printf("size of a int:%i byte(s)\n", sizeof(int));
+printf("size of a long int:%i byte(s)\n", sizeof(long int));
+printf("size of a long long int:%i byte(s)\n", sizeof(long long int));
+printf("size of a float:%i byte(s)\n", sizeof(float));
 return (0);
 }
-

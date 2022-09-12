9373eb4933483ea6ea8527a9f4dd40e524ecc0f6
Usman Adepoju <adepojuusman@gmail.com>
2022-09-11 18:32:11 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("size of a float: %d byte(s)\n", sizeof(float));
-}
 return (0);
+
+}

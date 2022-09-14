d9b46484f7b750dcd324cf8835d34557657500c2
Usman Adepoju <adepojuusman@gmail.com>
2022-09-13 18:15:47 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: print lowercase alphabet a to z
+ * Function alpha loop through the alphabet
+ * Return: 0;
+ */
+int main(void)
+{
+void alpha(void)
+{
+	char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+_putchar('\n);
+}
+return (void);
+}
+return 0;
+}

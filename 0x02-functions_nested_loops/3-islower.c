15e0f07f3cb596f23c30012c4f3bf1eb30933b00
Usman Adepoju <adepojuusman@gmail.com>
2022-09-13 20:42:49 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+/**
+ * _islower - checks for lowercase character
+ * @c:
+ * Return: islower
+ */
+int _islower(int c)
+{
+	if (c == 'c')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}

d21200490d5658ed4e2fe7711dda6738caffe9ab
Usman Adepoju <adepojuusman@gmail.com>
2022-09-15 08:52:02 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_numbers - function that prints
+ * Return;
+ */
+void print_numbers(void)
+{
+char a;
+for (a = 0; a <= 9; a++)
+_putchar(a);
+}

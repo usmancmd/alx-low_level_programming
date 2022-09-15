e802fa5399a386d77f036840eba17b053dd555df
Usman Adepoju <adepojuusman@gmail.com>
2022-09-15 03:53:46 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+/**
+ * _isupper -  function that checks for uppercase character.
+ * @c:  uppercase character.
+ * Return: (n);
+ */
+int _isupper(int c)
+	{
+	if (c <= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}

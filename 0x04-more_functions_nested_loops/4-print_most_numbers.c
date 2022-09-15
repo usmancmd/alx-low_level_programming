d9c91261548ccf98d889d164b201edbefaf4a20e
Usman Adepoju <adepojuusman@gmail.com>
2022-09-15 16:58:23 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print
+ * Return;
+ */
+void print_most_numbers(void)
+{
+char a;
+for (a = '0'; a <= '9'; a++)
+{
+if (a != '2' && a != '4')
+_putchar(a);
+}
+_putchar('\n');
+}

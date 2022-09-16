63a0abe7c22752164d59477d511e37c51a5c5055
Usman Adepoju <adepojuusman@gmail.com>
2022-09-16 10:38:20 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return (0);
+ */
+int main()
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a = a / 3)
+		printf("Fizz");
+		else if (a = a / 5)
+		printf("Buzz")
+		else (a = a / 3 && a = a / 5)
+		printf("FizzBuzz")
+		printf(" ");
+	}
+}

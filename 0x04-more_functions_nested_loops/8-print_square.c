70e43476e11ed79beef1fc90827548a586f65f7b
Usman Adepoju <adepojuusman@gmail.com>
2022-09-16 10:21:43 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,18 +2,20 @@
 /**
  * print_square - function that prints a square
  * @size: int size
- * return (size);
+ * return: void
  */
 void print_square(int size)
 {
 	int i, j;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 				_putchar('#');
-				 _putchar('\n');
+			 _putchar('\n');
 		}
 }

7968549c7defedb52a78a54c17e0dbb15bf37066
Usman Adepoju <adepojuusman@gmail.com>
2022-09-16 10:01:24 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - function that prints a square
+ * @size: int size
+ * return (size);
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar('#');
+				 _putchar('\n');
+		}
+	}
+}

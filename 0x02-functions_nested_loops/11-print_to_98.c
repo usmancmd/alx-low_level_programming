75e6b43a15816fe5662018f8ae1942e8c2fd235c
Usman Adepoju <adepojuusman@gmail.com>
2022-09-17 06:15:44 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+for (a = n; a <= 98; a++)
+	{
+	printf("%d", a);
+	if (a != 98)
+	printf(", ");
+	}
+	printf("\n");
+	return (0);
+}

7e38a4db462543cc4ccc7776ee385344017d90d5
Usman Adepoju <adepojuusman@gmail.com>
2022-09-17 05:17:31 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,9 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = n % 10;
-	if (last_digit < 0)
-	{
-	return (0 - last_digit);
-	}
-	else
-	{
-	return (last_digit);
-	}
+	last_digit = x % 10;
+	last_digit = last_digit < 0 ? 0 - last_digit : last_digit;
+
 	_putchar(last_digit + '0');
 
 	return (last_digit);

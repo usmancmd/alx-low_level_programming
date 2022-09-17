f69b9be696537b9a2dd960495395312b92e051e4
Usman Adepoju <adepojuusman@gmail.com>
2022-09-17 04:43:18 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of x
+ * @x: parameter
+ *
+ * Return: the distance from 0 to x
+ */
+int _abs(int x)
+{
+	return (x < 0 ? 0 - x : x);
+}

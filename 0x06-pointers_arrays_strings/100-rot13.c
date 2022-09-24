2507a73d9538c78e736d005e33cfb64ffde15d94
Adepoju Usman <adepojuusman465@gmail.com>
2022-09-24 20:20:15 +0000

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,27 @@
 #include "main.h"
+/**
+* rot13 - encodes using rot13
+* @s: string to encode
+*
+* Return: string
+*/
+char *rot13(char *s)
+{
+	int i, j;
+	char rot1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+		for (j = 0; j != '\n'; j++)
+		{
+			if (s[i] == rot1[j])
+			{
+			s[i] = rot2[j];
+			break;
+			}
+		}
+	}
+
+	return (0);
+}

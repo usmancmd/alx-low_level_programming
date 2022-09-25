4ac0eab834b5c301281ce389faa76a713b8f7288
Adepoju Usman <adepojuusman465@gmail.com>
2022-09-25 06:42:18 +0000

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_number - print an integer
+* @n: integer
+*
+*  Return: void
+*/
+void print_number(int n)
+{
+	_putchar(n);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
-#define MAIN_H	
+#define MAIN_H
 
+#include <stdio.h>
+
+int _putchar(char c);
 char *rot13(char *s);
+void print_number(int n);
 
 #endif

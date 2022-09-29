52d6f4cb1a7ab2cef70d8fde3063ea9ab1c9e82e
Usman Adepoju <adepojuusman@gmail.com>
2022-09-28 20:22:54 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* is_prime_number - function that returns 1 if prime otherwise 0
+* @n: parameter
+*
+* Return: 0
+*/
+int is_prime_number(int n)
+{
+	if (n % 10 == 0)
+	return (0);
+	else
+	{
+	return (is_prime_number(n % 10));
+	}
+}

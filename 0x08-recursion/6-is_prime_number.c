5c6719f56b81d50b3642b72c3ac1f333a151be71
Adepoju Usman <adepojuusman465@gmail.com>
2022-09-29 11:23:16 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,10 +7,22 @@
 */
 int is_prime_number(int n)
 {
-	if (n % 2 == 0)
-	return (0);
+	if (n <= 1)
+		return (0);
 	else
-	{
-	return (is_prime_number(n % 2));
-	}
+		return (prime(n));
+}
+
+/**
+* prime - find prime number
+* @n: number to check
+*
+* Return: 0
+*/
+int prime(int n)
+{
+	if (n % 2 == 1)
+		return (1);
+	else
+		return (0);
 }

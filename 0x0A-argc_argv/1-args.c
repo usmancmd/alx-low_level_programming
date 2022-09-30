b8c68df067cbae4addf65595e353284d6c7ea38b
Usman Adepoju <adepojuusman@gmail.com>
2022-09-30 15:50:34 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - starting point
+* @argc: argument count
+* @argv: argument array
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+return (0);
+}

63c4267778469703dd5c2eccffe9a2222134ef1e
Usman Adepoju <adepojuusman@gmail.com>
2022-09-30 17:16:35 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,28 +3,23 @@
 /**
 * main - entry point
 * @argc: argument count
-* @argv: argument array
+* @argv: argument vector
 *
 * Return: 0
 */
 int main(int argc, char *argv[])
 {
-	if (argc > 1 && < 3)
-{
-	int i;
-
-	for (i = 0; i <= argc; i++)
-	{
-	int mul = 1;
-
-	mul *= atoi(argv[i]);
-	printf("%d\n", mul);
-	}
-}
-	else
+	if (argc < 3)
 {
 	printf("%s\n", "Error");
 	return (1);
+}
+	else
+{
+	int mul;
+
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
 }
 return (0);
 }

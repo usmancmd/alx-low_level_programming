56363fff72419ca2b7694d6971a8e96bec209832
Adepoju Usman <adepojuusman465@gmail.com>
2022-10-11 10:25:56 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,11 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	{
 		d->name = name;
-		(*d).age = age;
+		d->age = age;
 		d->owner = owner;
 	}
 }

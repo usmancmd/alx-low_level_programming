729202f9c5e1c704fc39a38f98b4c3e1dd78fd55
Adepoju Usman <adepojuusman465@gmail.com>
2022-10-18 23:45:53 +0000

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,34 +15,37 @@ char *s;
 int j = 0, len = 0;
 
 	va_start(argPtr, format);
-	while (format[len])
-		len++;
-	while (format[j])
+	if (format)
 	{
-		switch (format[j])
+		while (format[len])
+			len++;
+		while (format[j])
 		{
-			case 'c':
-			printf("%c", (char) va_arg(argPtr, int));
-			break;
-			case 'i':
-			printf("%i", va_arg(argPtr, int));
-			break;
-			case 'f':
-			printf("%f", (float) va_arg(argPtr, double));
-			break;
-			case 's':
-			s = va_arg(argPtr, char *);
-			if (s == NULL)
-			s = "(nil)";
-			printf("%s", s);
-			break;
-			default:
-				j++;
-				continue;
-		}
+			switch (format[j])
+				{
+				case 'c':
+				printf("%c", (char) va_arg(argPtr, int));
+				break;
+				case 'i':
+				printf("%i", va_arg(argPtr, int));
+				break;
+				case 'f':
+				printf("%f", (float) va_arg(argPtr, double));
+				break;
+				case 's':
+				s = va_arg(argPtr, char *);
+				if (s == NULL)
+				s = "(nil)";
+				printf("%s", s);
+				break;
+				default:
+					j++;
+					continue;
+				}
 		if (j < (len - 1))
 		printf(", ");
 		j++;
+		}
 	}
 	printf("\n");
 	va_end(argPtr);

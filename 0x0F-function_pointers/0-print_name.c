83bc36b4b2ce543887f905497db6937881a91764
Adepoju Usman <adepojuusman465@gmail.com>
2022-10-19 09:24:24 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,8 +9,8 @@
 * Return: void
 */
 
-vioid print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }

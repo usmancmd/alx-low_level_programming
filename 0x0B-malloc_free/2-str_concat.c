e3067ceac9e592b0675fd6481abafb4d50ef6bbd
Adepoju Usman <adepojuusman465@gmail.com>
2022-10-19 03:01:55 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,4 @@
-  #include <stdlib.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -6,7 +6,7 @@
  * @s1: string to concatenate
  * @s2: other string to concatenate
  *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * Return: pointer to the new string created 
  */
 char *str_concat(char *s1, char *s2)
 {

fce877b61f0482c3d13c5ede7195398e26d03ada
Adepoju Usman <adepojuusman465@gmail.com>
2022-10-19 09:06:38 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,9 +10,9 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *s3;
-unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])
@@ -20,18 +20,18 @@ unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
-		return (NULL);
+	return (NULL);
 
 	i = 0;
 	j = 0;
-			
+
 	if (s1)
 	{
 		while (i < len1)
-	{
-	s3[i] = s1[i];
-	i++;
-	}
+		{
+		s3[i] = s1[i];
+			i++;
+		}
 	}
 
 	if (s2)
@@ -39,11 +39,11 @@ unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 	while (i < (len1 + len2))
 	{
 	s3[i] = s2[j];
-	i++;
-	j++;
+		i++;
+		j++;
 	}
 	}
 	s3[i] = '\0';
 
-	return (s3);
+return (s3);
 }

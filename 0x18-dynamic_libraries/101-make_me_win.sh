0b79928940014bc72fe230fac2b513580ed69aca
usmancmd <110719750+usmancmd@users.noreply.github.com>
2022-12-13 05:46:21 +0100

    commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/usmancmd/alx-low_level_programming/main/0x18-dynamic_libraries/random.so
-export LD_PRELOAD=/tmp/random.so
+wget -P /tmp/ https://raw.github.com/usmancmd/alx-low_level_programming/main/0x18-dynamic_libraries/librandom.so
+export LD_PRELOAD=/tmp/librandom.so

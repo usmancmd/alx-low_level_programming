b82d78310be73f91d1cb45e48291eb1d48eb8787
Adepoju Usman <adepojuusman465@gmail.com>
2023-01-06 08:56:48 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,26 +8,21 @@
 *
 * Return: a pointer to the newly created hash table
 */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_node_t));
-
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_table_t));
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
-
 	for (i = 0; i < size; i++)
-	{
 		ht->array[i] = NULL;
-	}
+
 	return (ht);
 }

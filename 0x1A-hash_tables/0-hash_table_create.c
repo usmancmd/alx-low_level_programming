4b20bbc5fba54dec76f3a66b29b4355c1150189b
Usman Adepoju <adepojuusman465gmail.com>
2023-01-09 00:03:32 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,9 +16,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	ht->array = malloc(sizeof(hash_table_t) * size);
+	if (ht == NULL)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
 
